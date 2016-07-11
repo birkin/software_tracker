555350a861370124e08edf9276a85a9f752dcc5a
birkin <birkin.diana@gmail.com>
2016-07-11 09:21:14 -0400

    adds admin; can now add an entry.

--- software_tracker/admin.py
+++ software_tracker/admin.py
@@ -2,3 +2,16 @@
 
 from __future__ import unicode_literals
 from django.contrib import admin
+from software_tracker.models import Software
+
+
+class SoftwareAdmin(admin.ModelAdmin):
+  ordering = [ 'name' ]
+  list_display = [ 'name', 'audience', 'activity', 'contact_technical_email' ]
+  list_filter = [ 'activity', 'api', 'audience', 'license_name' ]
+  search_fields = [ 'name' ]
+  prepopulated_fields = { u'slug': (u'name',) }
+  # readonly_fields = [ 'code', 'settlement_code', 'region_code' ]
+
+
+admin.site.register( Software, SoftwareAdmin )
