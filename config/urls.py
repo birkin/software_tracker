b51d6de9257f2dc794ea66f1fa69af0014df3f5e
birkin <birkin.diana@gmail.com>
2016-08-10 12:06:12 -0400

    updates urls.

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+
+urlpatterns = [
 
     url( r'^software/admin/', include(admin.site.urls) ),  # eg host/project_x/admin/
 
     url( r'^software/', include('software_tracker.urls_app') ),  # eg host/project_x/anything/
 
-)
+]
--- software_tracker/urls_app.py
+++ software_tracker/urls_app.py
@@ -1,16 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-from django.conf.urls import patterns, include, url
+# from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from django.views.generic import RedirectView
+from software_tracker import views
 
 
-urlpatterns = patterns('',
+urlpatterns = [
 
-    url( r'^info/$',  'software_tracker.views.hi', name='info_url' ),
+    url( r'^info/$',  views.hi, name='info_url' ),
 
-    url( r'^apps/$', 'software_tracker.views.apps2', name='apps_url' ),
+    url( r'^apps/$', views.apps2, name='apps_url' ),
 
     url( r'^$',  RedirectView.as_view(pattern_name='apps_url') ),
 
-    )
+    ]
+
+
+# urlpatterns = patterns('',
+
+#     url( r'^info/$',  'software_tracker.views.hi', name='info_url' ),
+
+#     url( r'^apps/$', 'software_tracker.views.apps2', name='apps_url' ),
+
+#     url( r'^$',  RedirectView.as_view(pattern_name='apps_url') ),
+
+#     )
+
